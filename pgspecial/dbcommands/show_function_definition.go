a92be4914fd599c01462d74bf89d67923968610c
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-07 17:29:32 +0530

    feat: add show func definition command

---
+++ pgspecial/dbcommands/show_function_definition.go
@@ -0,0 +1,71 @@
+package dbcommands
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/balaji01-4d/pgspecial/pgspecial"
+	"github.com/balaji01-4d/pgspecial/pgspecial/database"
+	"github.com/jackc/pgx/v5"
+)
+
+func init() {
+	pgspecial.RegisterCommand(pgspecial.SpecialCommandRegistry{
+		Cmd:           "\\sf",
+		Description:   "Show a function's definition.",
+		Syntax:        "\\sf[+] FUNCNAME",
+		Handler:       ShowFunctionDefinition,
+		CaseSensitive: true,
+	})
+}
+
+func ShowFunctionDefinition(ctx context.Context, db database.DB, pattern string, verbose bool) (pgx.Rows, error) {
+	var sql string
+	if strings.Contains(pattern, "(") {
+		sql = "SELECT $1::pg_catalog.regprocedure::pg_catalog.oid"
+	} else {
+		sql = "SELECT $1::pg_catalog.regproc::pg_catalog.oid"
+	}
+
+	var foid uint32
+	err := db.QueryRow(ctx, sql, pattern).Scan(&foid)
+	if err != nil {
+		return nil, err
+	}
+
+	sql = "SELECT pg_catalog.pg_get_functiondef($1) as source"
+	if !verbose {
+		return db.Query(ctx, sql, foid)
+	}
+
+	var source string
+	err = db.QueryRow(ctx, sql, foid).Scan(&source)
+	if err != nil {
+		return nil, err
+	}
+
+	var sb strings.Builder
+	lines := strings.Split(source, "\n")
+	var rown int
+	started := false
+
+	for _, row := range lines {
+		var prefix string
+		if !started {
+			if strings.HasPrefix(row, "AS ") {
+				started = true
+				rown = 1
+				prefix = fmt.Sprintf("%-7d", rown)
+			} else {
+				prefix = fmt.Sprintf("%-7s", "")
+			}
+		} else {
+			rown++
+			prefix = fmt.Sprintf("%-7d", rown)
+		}
+		sb.WriteString(prefix + " " + row + "\n")
+	}
+
+	return db.Query(ctx, "SELECT $1 as source", sb.String())
+}
