5468c711eda341c23ec4ddd2a0b6ed2f7f02fb32
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-09 22:42:17 +0530

    fix: remove unnecessary close()

--- database/queryer.go
+++ database/queryer.go
@@ -13,5 +13,4 @@ import (
 type Queryer interface {
 	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
-	Close()
 }
