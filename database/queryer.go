eff0be0287f59df409f2225510411711c559e1fe
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-09 22:47:43 +0530

    docs: update docs

--- database/queryer.go
+++ database/queryer.go
@@ -9,7 +9,8 @@ import (
 
 // Queryer is an interface that defines methods for querying a database.
 // It includes methods for executing queries that return multiple rows, single rows, and closing the connection.
-// test with pgx.Conn and pgxpool.Pool
+// test with pgx.Conn and pgxpool.Pool.
+// suitable pgx ecosystem
 type Queryer interface {
 	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
